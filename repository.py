6f751838f6176214017e3bb86e22f8a0affb976a
Yana <146076288+yanalebedynska@users.noreply.github.com>
2024-10-24 16:47:52 +0300

    Create repository.py
    Added base repository and some repositories for objects

---
+++ repository.py
@@ -0,0 +1,47 @@
+from django.core.exceptions import ObjectDoesNotExist
+from .models import (
+    Employee, EmployeeRole, Supplier, ConditionOfRecord, Record,
+    StoreUser, ShippingAddress, UserShippingAddress, CreditCard,
+    OrderInStore, RecordInOrder, TransactionInOrder
+)
+
+# Базовий клас для репозиторіїв з інкапсуляцією
+class BaseRepository:
+    _model = None  # захищений атрибут для моделі
+
+    def get_all(self):
+        """Отримати всі записи моделі"""
+        return self._model.objects.all()
+
+    def get_by_id(self, pk):
+        """Отримати запис по ID (захищений метод)"""
+        return self._get_by_id(pk)
+
+    def create(self, **kwargs):
+        """Створити новий запис"""
+        return self._model.objects.create(**kwargs)
+
+    def _get_by_id(self, pk):
+        """Захищений метод для пошуку по ID"""
+        try:
+            return self._model.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return None
+
+
+# Репозиторій для EmployeeRole
+class EmployeeRoleRepository(BaseRepository):
+    def __init__(self):
+        self._model = EmployeeRole  # Призначаємо модель через інкапсуляцію
+
+
+# Репозиторій для Employee
+class EmployeeRepository(BaseRepository):
+    def __init__(self):
+        self._model = Employee  # Призначаємо модель через інкапсуляцію
+
+
+# Інші репозиторії аналогічно
+class SupplierRepository(BaseRepository):
+    def __init__(self):
+        self._model = Supplier
