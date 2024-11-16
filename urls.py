340913d560986ec9ecdc73bd133ba7eb2f7185ab
MariaBrychko <146076221+MariaBrychko@users.noreply.github.com>
2024-11-16 11:03:40 +0200

    pharmacyApp, else

---
+++ urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from pharmacyApp.views import (
+    EmployeeViewSet, CompanyViewSet, CustomerViewSet, OrderViewSet, PharmacyViewSet,
+    ProductViewSet, ReceiptViewSet, ReceiptItemViewSet, SupplierViewSet, TransactionViewSet,
+    WarehouseViewSet, WarehouseStockViewSet
+)
+
+router = DefaultRouter()
+router.register(r'employees', EmployeeViewSet, basename='employee')
+router.register(r'companies', CompanyViewSet, basename='company')
+router.register(r'customers', CustomerViewSet, basename='customer')
+router.register(r'orders', OrderViewSet, basename='order')
+router.register(r'pharmacies', PharmacyViewSet, basename='pharmacy')
+router.register(r'warehouses', WarehouseViewSet, basename='warehouse')
+router.register(r'warehousestocks', WarehouseStockViewSet, basename='warehousestock')
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'receipts', ReceiptViewSet, basename='receipt')
+router.register(r'receiptitems', ReceiptItemViewSet, basename='receiptitem')
+router.register(r'suppliers', SupplierViewSet, basename='supplier')
+router.register(r'transactions', TransactionViewSet, basename='transaction')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
